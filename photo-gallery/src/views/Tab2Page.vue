bdd08662b430b9413f3d1506c6dee1cfe109a53b
kendra.jade <kindrai.kendra@gmail.com>
2025-06-12 10:12:01 -0500

    Set things up and am about to start using compositionAPI

---
+++ photo-gallery/src/views/Tab2Page.vue
@@ -0,0 +1,37 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <!-- Content Section -->
+    <ion-content :fullscreen="true">
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="takePhoto()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+    
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { camera, trash, close } from 'ionicons/icons';
+import { 
+  IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue';
+</script>
